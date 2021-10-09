fcf920ec648b38f696afba9b27992583990e12f8
jing <yjli98@qq.com>
2021-10-09 14:13:29 +0800

    代码整理

--- read_sim_data.py
+++ read_sim_data.py
@@ -26,65 +26,45 @@ def hsv_to_rgb(h, s, v):
 
 
 def read_sim_data():
-    zoom_in = 20
+    zoom_in = 20  # 视频的放大倍数
+    radius = int(0.38 * zoom_in)  # 调整行人的身体半径显示大小
     for frame_indx in range(1, 900):  # 控制读几帧画面
         with open('sim_data/' + str(frame_indx) + '.xml', 'r') as fp:
-            agents = []
-            for line in fp.readlines()[3:-2]:
+            agents = []  # 存储每个agent的画图信息
+            mags = []  # 用于后续对速度进行正则化显示
+            for line in fp.readlines()[3:-2]:  # 读取xml内容
                 velocity = line.split()[1] + line.split()[2]
                 velocity = velocity[velocity.index('{') + 1:velocity.index('}')].split(';')
                 velocity = [float(i) for i in velocity]
                 position = line.split()[3] + line.split()[4]
                 position_x = position[position.index('x') + 3:position.index('x') + 7]
                 position_y = position[position.index('y') + 3:position.index('y') + 7]
-                position = [float(position_x), float(position_y)]
-                agents.append(position + velocity)
-                # break
-            # print(agents)
-            # 接下来对agent数据的操作包括
-            # position到像素的整理
-            # 速度到HSV图像的转换
-            # agents = [[6,5,1,0],[4,5,-1,0],[5,6,0,1],[5,4,0,-1],[4,6,-1,-1],[4,4,-1,1]]  # 用于调试色彩空间
-            background = np.zeros((zoom_in * 10, zoom_in * 10, 3))
-            # rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-            # x = np.random.randint(0,480)
-            # y = np.random.randint(0,480)
-            for agent in agents:
-                # scale position 0-10 to 0-480
-                x = int(agent[0] * zoom_in)
-                y = int(agent[1] * zoom_in)
+                # 接下来对agent数据的操作包括
+                # position到像素的整理
+                # 速度到HSV图像的转换
+                x = int(float(position_x) * zoom_in)  # scale position 0-10 to 0-480
+                y = int(float(position_y) * zoom_in)
                 # scale velocity
                 # turn velocity into color
-                vx = agent[2]
-                vy = agent[3]
-                # cn = complex(vx, vy)
-                # print(cmath.polar(cn))
-                # mag, ang = cv2.cartToPolar(vx, vy)
-                mag = np.sqrt(vy ** 2 + vx ** 2)
-                if round(mag, 4) == 0:
-                    r, g, b = (0, 0, 0)
-                else:
-                    cn = complex(vx, vy)
-                    mag, ang = cmath.polar(cn)
-                    # ang = ang * (180 / np.pi)
-                    # ang = (ang + 360) % 360
-                    # if ang < 0:
-                    #     print(ang)
-                    #     ang += 360
-                    # hsv = [255] * 3
-                    # hsv[0] = ang * 180 / np.pi / 2
-                    # hsv[2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
-                    # print(vx, vy)
-                    mag = max(0, min(mag * 2, 1))
-                    # print(mag)
-                    r, g, b = hsv_to_rgb(ang, 1.0, mag)
-                    print(agent, mag, ang, r, g, b)
-                radius = int(0.4 * zoom_in)
+                vx = velocity[0]
+                vy = velocity[1]
+                cn = complex(vx, vy)
+                mag, ang = cmath.polar(cn)
+                mags.append(mag)
+                agents.append([(x, y), ang, mag])
 
-                cv2.circle(background, (x, y), radius, (b, g, r), -1)
+            background = np.zeros((zoom_in * 10, zoom_in * 10, 3))  # 创建黑色背景 10为在仿真中截取的大小
+            max_mag, min_mag = np.max(mags), np.min(mags)
+            # mu, sigma = np.mean(mags),np.std(mags)
+            for data in agents:
+                posi, ang, mag = data
+                mag = (mag - min_mag) / (max_mag - min_mag)
+                # mag = (mag - mu) / sigma
+                r, g, b = hsv_to_rgb(ang, 1.0, mag)
+                cv2.circle(background, posi, radius, (b, g, r), -1)
 
             cv2.imshow('frame2', background)
-            k = cv2.waitKey(10) & 0xff
+            k = cv2.waitKey(1) & 0xff
             if k == ord('q') or k == ord(' '):  # quit
                 break
 
