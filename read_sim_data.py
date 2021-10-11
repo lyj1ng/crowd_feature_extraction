2736fb9eebfd6fff31873299a1d10ddd9b87e9e2
jing <yjli98@qq.com>
2021-10-11 19:42:19 +0800

    更新了对仿真结果的支持

--- read_sim_data.py
+++ read_sim_data.py
@@ -25,15 +25,19 @@ def hsv_to_rgb(h, s, v):
     if i == 5: return v, p, q
 
 
-def read_sim_data():
+def read_sim_data(folder='sim_data'):
     zoom_in = 20  # 视频的放大倍数
+    # record size 形为 ( y , x )
+    record_size = (10, 10)
+    # record_size = (18, 100)
     radius = int(0.38 * zoom_in)  # 调整行人的身体半径显示大小
 
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter('./sim_to_optical_flow.avi', fourcc, 10, (int(10 * zoom_in), int(10 * zoom_in)))
+    out = cv2.VideoWriter('./sim_to_optical_flow.avi', fourcc, 10,
+                          (int(record_size[0] * zoom_in), int(record_size[1] * zoom_in)))
 
     for frame_indx in range(1, 900):  # 控制读几帧画面
-        with open('sim_data/' + str(frame_indx) + '.xml', 'r') as fp:
+        with open(folder + '/' + str(frame_indx) + '.xml', 'r') as fp:
             agents = []  # 存储每个agent的画图信息
             mags = []  # 用于后续对速度进行正则化显示
             for line in fp.readlines()[3:-2]:  # 读取xml内容
@@ -58,10 +62,11 @@ def read_sim_data():
                 # agents.append([(x, y), ang, mag])
                 # ang /= 3.14*2
                 # ang += 1
-                ang = (ang/(3.1416*2) + 1) % 1
-                agents.append([(x, y), ang, mag, velocity, position,hsv_to_rgb(ang, 1.0, mag)])
+                ang = (ang / (3.1416 * 2) + 1) % 1
+                agents.append([(x, y), ang, mag, velocity, position, hsv_to_rgb(ang, 1.0, mag)])
 
-            background = np.zeros((zoom_in * 10, zoom_in * 10, 3), np.uint8)  # 创建黑色背景 10为q在仿真中截取的大小
+            background = np.zeros((zoom_in * record_size[0], zoom_in * record_size[1], 3),
+                                  np.uint8)  # 创建黑色背景 10为q在仿真中截取的大小
             max_mag, min_mag = np.max(mags), np.min(mags)
             # mu, sigma = np.mean(mags),np.std(mags)
             for data in agents:
@@ -89,7 +94,7 @@ def read_sim_data():
                 flow = cv2.calcOpticalFlowFarneback(prev_frame[:, :, 2], background[:, :, 2], None, 0.5, 3, 15, 3, 7,
                                                     1.5, 0)
                 mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
-                hsv = np.zeros((10 * zoom_in, 10 * zoom_in, 3), np.uint8)
+                hsv = np.zeros((record_size[0] * zoom_in, record_size[1] * zoom_in, 3), np.uint8)
                 hsv[..., 1] = 255
                 hsv[..., 0] = ang * 180 / np.pi / 2
                 hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
@@ -116,4 +121,5 @@ def read_sim_data():
 
 
 if __name__ == '__main__':
+    # read_sim_data(folder='full_size')
     read_sim_data()
