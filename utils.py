79f83f699327ad1df4cb756428d72f926f8aa186
jing <yjli98@qq.com>
2021-10-11 16:39:57 +0800

    增加有关信息量的计算

--- utils.py
+++ utils.py
@@ -47,4 +47,52 @@ def constrain_max_velocity(velo=[0, 0], threshold=1000):
         return True
 
 
-# print(cosine_similarity([1, 2], [0, 0]))
+def calc_ent(x):
+    """
+        calculate shanno ent of x
+    """
+
+    x_value_list = set([x[i] for i in range(x.shape[0])])
+    ent = 0.0
+    for x_value in x_value_list:
+        p = float(x[x == x_value].shape[0]) / x.shape[0]
+        logp = np.log2(p)
+        ent -= p * logp
+
+    return ent
+
+
+def calc_condition_ent(x, y):
+    """
+        calculate ent H(y|x)
+    """
+
+    # calc ent(y|x)
+    x_value_list = set([x[i] for i in range(x.shape[0])])
+    ent = 0.0
+    for x_value in x_value_list:
+        sub_y = y[x == x_value]
+        temp_ent = calc_ent(sub_y)
+        ent += (float(sub_y.shape[0]) / y.shape[0]) * temp_ent
+
+    return ent
+
+
+def calc_ent_grap(x, y):
+    """
+        calculate ent grap
+    """
+
+    base_ent = calc_ent(y)
+    condition_ent = calc_condition_ent(x, y)
+    ent_grap = base_ent - condition_ent
+
+    return ent_grap
+
+
+if __name__ == '__main__':
+    a = [0, 0, 0, 0, 0, 0, 1, 2]
+    print(calc_ent(np.array(a)))
+    b = [1, 1, 1, 1, 1, 1, 2, 2]
+    print(calc_ent_grap(np.array(a), np.array(b)))
+    # print(cosine_similarity([1, 2], [0, 0]))
