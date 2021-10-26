935ef7858e20b116b29938ba23b747fac7f75487
jing <yjli98@qq.com>
2021-10-26 16:04:05 +0800

    对仿真数据的直接可视化输出展示

--- show_sim_data.py
+++ show_sim_data.py
@@ -39,7 +39,7 @@ def show_sim_data(folder='sim_data'):
     for frame_indx in range(1, 900):  # 控制读几帧画面
         with open(folder + '/' + str(frame_indx) + '.xml', 'r') as fp:
             agents = []  # 存储每个agent的画图信息
-            mags = []  # 用于后续对速度进行正则化显示
+            # mags = []  # 用于后续对速度进行正则化显示
             for line in fp.readlines()[3:-2]:  # 读取xml内容
                 velocity = line.split()[1] + line.split()[2]
                 velocity = velocity[velocity.index('{') + 1:velocity.index('}')].split(';')
@@ -47,44 +47,23 @@ def show_sim_data(folder='sim_data'):
                 position = line.split()[3] + line.split()[4]
                 position_x = position[position.index('x') + 3:position.index('x') + 7]
                 position_y = position[position.index('y') + 3:position.index('y') + 7]
-                # 接下来对agent数据的操作包括
-                # position到像素的整理
-                # 速度到HSV图像的转换
+
                 x = int(float(position_x) * zoom_in)  # scale position 0-10 to 0-480
                 y = int(float(position_y) * zoom_in)
-                # scale velocity
-                # turn velocity into color
-                vx = velocity[0]
-                vy = -1 * velocity[1]
-                cn = complex(vx, vy)
-                mag, ang = cmath.polar(cn)
-                mags.append(mag)
-                # agents.append([(x, y), ang, mag])
-                # ang /= 3.14*2
-                # ang += 1
-                ang = (ang / (3.1416 * 2) + 1) % 1
-                agents.append([(x, y), ang, mag, velocity, position, hsv_to_rgb(ang, 1.0, mag)])
+
+                agents.append([(x, y), 0, 0, velocity, position, (0,0,0)])
 
             background = np.full((zoom_in * record_size[0], zoom_in * record_size[1], 3),
                                   255,np.uint8)  # 创建黑色背景 10为q在仿真中截取的大小
-            max_mag, min_mag = np.max(mags), np.min(mags)
+            # max_mag, min_mag = np.max(mags), np.min(mags)
             # mu, sigma = np.mean(mags),np.std(mags)
             for data in agents:
                 posi = data[0]
-                ang = data[1]
-                mag = data[2]
-                if max_mag == min_mag:
-                    mag = 0
-                else:
-                    mag = (mag - min_mag) / (max_mag - min_mag)
-                mag *= 3
-                # mag = (mag - mu) / sigma
-                r, g, b = hsv_to_rgb(ang, 1.0, mag)
-                r, g, b = int(r * 255), int(g * 255), int(b * 255)
-                # 记录一下这里：如果图片格式是float，那么范围在0-1；如果图片格式为int，范围在0-255。
-                # 为了后面保存 or 其他操作的需要（因为opencv无法操作float64），这里将float转为int因此要乘上255
-                b,g,r = 10,10,10
+
+                b,g,r = 200,200,200  # body color
+                head = (200,100,100)  # head color
                 cv2.circle(background, posi, radius, (b, g, r), -1)
+                cv2.circle(background, posi, int(radius*0.5), head, -1)
 
             # 至此
             # 每帧 仿真 对应 的 运动场 渲染 完毕 ：cv2.imshow('frame xxx', background)可以进行输出
@@ -111,7 +90,7 @@ def show_sim_data(folder='sim_data'):
             else:
                 out.write(background)
                 cv2.imshow('frame1', background)
-            k = cv2.waitKey(30) & 0xff
+            k = cv2.waitKey(10) & 0xff
             if k == ord('q') or k == ord(' '):  # quit
                 print(frame_indx)
                 # debug information
