84866517198e94eaeb426bff4b19da85236893ee
forev <yjli98@qq.com>
2022-05-10 14:25:34 +0800

    test for graph-file

--- test_data.py
+++ test_data.py
@@ -2,4 +2,5 @@ import numpy as np
 
 data = np.load('graph_save/02/00161.npz')
 data = np.load('graph_save/umn/test01/00161.npz')
+data = np.load('graph_save/hajj/01/00062.npz')
 print(data['f'].shape)
