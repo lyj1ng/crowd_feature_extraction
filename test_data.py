7be3084dc6200b2519a150495d37ae7fb243de99
forev <yjli98@qq.com>
2022-05-11 22:16:42 +0800

    small update

--- test_data.py
+++ test_data.py
@@ -3,4 +3,28 @@ import numpy as np
 data = np.load('graph_save/02/00161.npz')
 data = np.load('graph_save/umn/test01/00161.npz')
 data = np.load('graph_save/hajj/01/00062.npz')
-print(data['f'].shape)
+print('data : node feature size (node count, feature dim) :', data['f'].shape)
+
+print('data : adj2 size : ', data['a2'].shape)
+print('total sum:', len(np.where(data['a2'] >= 0)[0]))
+print('zero sum:', len(np.where(data['a2'] == 0)[0]))
+print('one sum:', len(np.where(data['a2'] == 1)[0]))
+print(data['a2'])
+print('-'*20)
+print('data : adj size : ', data['a'].shape)
+print('total sum:', len(np.where(data['a'] >= 0)[0]))
+print('zero sum:', len(np.where(data['a'] == 0)[0]))
+print('one sum:', len(np.where(data['a'] == 1)[0]))
+print(data['a'])
+print('-'*20)
+test = data['a'] * data['a2']
+print('data : test size : ', test.shape)
+print('total sum:', len(np.where(test >= 0)[0]))
+print('zero sum:', len(np.where(test == 0)[0]))
+print('one sum:', len(np.where(test == 1)[0]))
+print(test)
+
+for i in range(data['a'].shape[0]):
+    for j in range(data['a'].shape[1]):
+        if data['a'][i,j]!=0:
+            print(data['a'][i,j],test[i,j])
